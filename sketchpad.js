5424ec26894b85c101023bb777d549c8c90f587d
Rene Flooren <floorenr@gmail.com>
2021-05-08 12:54:19 +0200

    Add class sketchblocks to all small divs

--- sketchpad.js
+++ sketchpad.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container')
 
 for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
+  div.classList.add('sketchblocks'); 
   container.appendChild(div);
 }
 
