65705827486719be790e477cc5b88095646f3f6f
Rene Flooren <floorenr@gmail.com>
2021-05-08 12:51:56 +0200

    create const divs and ref to all divs in HTML. Iterate through divs, add EventListeners mouseover and make div's background color black.

--- sketchpad.js
+++ sketchpad.js
@@ -4,3 +4,11 @@ for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
   container.appendChild(div);
 }
+
+const divs = document.querySelectorAll('div');
+
+divs.forEach((div) => {
+  div.addEventListener('mouseover', () => {
+    div.style.backgroundColor = 'black';
+  });
+});
