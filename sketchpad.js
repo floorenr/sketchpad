1684b4c604a6003ec7459aa030ba02236ec745d9
Rene Flooren <floorenr@gmail.com>
2021-05-08 12:28:35 +0200

    Change to 16x16 grid instead of 4x4

--- sketchpad.js
+++ sketchpad.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container')
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
   container.appendChild(div);
 }
